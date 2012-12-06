0a7f1508744b0bddbf31582d9074286c23dc79b8
Pinto Jérémy <jeremy.pinto82@gmail.com>
2012-12-07 00:43:32 +0100

    HEY COMMIT!

---
+++ script.sql
@@ -0,0 +1,36 @@
+insert into lieu (nom,description,media,lattitude,longitude,idDpt,idDom) 
+	values ("Place du capitole",
+			"Place centrale de Toulouse, lieu hautement touristique de la ville rose qui rassemble la mairie de la commune et de nombreux commerces.",
+			"...",
+			"43.604460",
+			"1.443210",
+			31,
+			1);
+
+insert into lieu (nom,description,media,lattitude,longitude,idDpt,idDom) 
+	values ("Tour Eiffel",
+			"Symbole de la France dans le monde entier, la tour Eiffel a été pensée par l'ingénieur Gustave Eiffel pour l'exposition universelle de 1889.",
+			"...",
+			"48.858292",
+			"2.294422",
+			75,
+			1);
+
+insert into lieu (nom,description,media,lattitude,longitude,idDpt,idDom) 
+	values ("Carcassonne",
+			"La ville est connue pour la Cité de Carcassonne, un ensemble architectural médiéval restauré par Viollet-le-Duc au XIXe siècle et inscrit au patrimoine mondial de l'UNESCO depuis 1997.",
+			"...",
+			"43.210931",
+			"2.352390",
+			11,
+			1);
+
+insert into lieu (nom,description,media,lattitude,longitude,idDpt,idDom) 
+	values ("Vieux-Port de Marseille",
+			"Le Vieux-Port (en provençal lo Pòrt Vielh) est le plus ancien port de Marseille, ainsi que le centre historique et culturel de la ville depuis sa fondation sur ce site durant l'Antiquité.",
+			"...",
+			"43.295254",
+			"5.373825",
+			13,
+			1);
+
